6c64e1c5fbf9cf27841793740dbdd8d61c175aea
AntonRufus <zhyvotovskyi@gmail.com>
2020-12-15 11:42:11 +0800

    fifth commit

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,236 +1,34 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
 import css from './Time.module.css'
 import Clock from "./clock/Clock";
 import Hello from "./Hello";
-import "./time.css"
 
-class Time extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-            color: ['red', 'orange', 'yellow', 'green', 'dodgerblue', 'blue', 'purple', 'black', 'white', 'grey',],
-            clock1color: 'red',
-            clock2color: 'orange',
-            clock3color: 'yellow',
-            clock4color: 'green',
-            clock5color: 'dodgerblue',
-            clock6color: 'blue',
-            clock7color: 'purple',
-            clock8color: 'black',
-            clock9color: 'white',
-            clock10color: 'grey',
-        }
-    }
+const Time = () => {
 
-    toggleSwitchClock1 = () => {
-        this.setState({
-            clock1: !this.state.clock1,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
+    const [ clock, setClock ] = useState(false);
+    const [ color, setColor ] = useState(null);
 
-    toggleSwitchClock2 = () => {
-        this.setState({
-            clock1: false,
-            clock2: !this.state.clock2,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
+    const ButtonConstructor = ({ name, colorBgc, colorText }) => <button
+            style={ {backgroundColor: colorBgc, marginRight: '22px', color: colorText} }
+            onClick={ () => {setColor(colorBgc); setClock(true)} }>
+                { name }
+            </button>
 
-    toggleSwitchClock3 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: !this.state.clock3,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock4 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: !this.state.clock4,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock5 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: !this.state.clock5,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock6 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: !this.state.clock6,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock7 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: !this.state.clock7,
-            clock8: false,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock8 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: !this.state.clock8,
-            clock9: false,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock9 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: !this.state.clock9,
-            clock10: false,
-        })
-    };
-
-    toggleSwitchClock10 = () => {
-        this.setState({
-            clock1: false,
-            clock2: false,
-            clock3: false,
-            clock4: false,
-            clock5: false,
-            clock6: false,
-            clock7: false,
-            clock8: false,
-            clock9: false,
-            clock10: !this.state.clock10,
-        })
-    };
-
-    render() {
-        return (
-            <div className={css.time}>
-                <button style={{backgroundColor: this.state.color[0], marginRight: '22px'}} onClick={this.toggleSwitchClock1}>
-                    RED
-                </button>
-                <button style={{backgroundColor: this.state.color[1], marginRight: '22px'}} onClick={this.toggleSwitchClock2}>
-                    ORANGE
-                </button>
-                <button style={{backgroundColor: this.state.color[2], marginRight: '22px'}} onClick={this.toggleSwitchClock3}>
-                    YELLOW
-                </button>
-                <button style={{backgroundColor: this.state.color[3], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock4}>
-                    GREEN
-                </button>
-                <button style={{backgroundColor: this.state.color[4], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock5}>
-                    LIGHT BLUE
-                </button>
-                <button style={{backgroundColor: this.state.color[5], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock6}>
-                    BLUE
-                </button>
-                <button style={{backgroundColor: this.state.color[6], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock7}>
-                    PURPLE
-                </button>
-                <button style={{backgroundColor: this.state.color[7], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock8}>
-                    BLACK
-                </button>
-                <button style={{backgroundColor: this.state.color[8], marginRight: '22px'}} onClick={this.toggleSwitchClock9}>
-                    WHITE
-                </button>
-                <button style={{backgroundColor: this.state.color[9], marginRight: '22px', color:'white'}} onClick={this.toggleSwitchClock10}>
-                    GREY
-                </button>
-                {!this.state.clock1 && !this.state.clock2 && !this.state.clock3 && !this.state.clock4
-                && !this.state.clock5 && !this.state.clock6 && !this.state.clock7 && !this.state.clock8
-                && !this.state.clock9 && !this.state.clock10 ? <Hello/> : ''}
-                {this.state.clock1 ? <Clock bgc={this.state.color[0]}/> : ''}
-                {this.state.clock2 ? <Clock bgc={this.state.color[1]}/> : ''}
-                {this.state.clock3 ? <Clock bgc={this.state.color[2]}/> : ''}
-                {this.state.clock4 ? <Clock bgc={this.state.color[3]}/> : ''}
-                {this.state.clock5 ? <Clock bgc={this.state.color[4]}/> : ''}
-                {this.state.clock6 ? <Clock bgc={this.state.color[5]}/> : ''}
-                {this.state.clock7 ? <Clock bgc={this.state.color[6]}/> : ''}
-                {this.state.clock8 ? <Clock bgc={this.state.color[7]}/> : ''}
-                {this.state.clock9 ? <Clock bgc={this.state.color[8]}/> : ''}
-                {this.state.clock10 ? <Clock bgc={this.state.color[9]}/> : ''}
-            </div>
-        );
-    }
+    return (
+        <div className={css.time}>
+            <ButtonConstructor name='RED'         colorBgc='red'         colorText='black' />
+            <ButtonConstructor name='ORANGE'      colorBgc='orange'      colorText='black' />
+            <ButtonConstructor name='YELLOW'      colorBgc='yellow'      colorText='black' />
+            <ButtonConstructor name='GREEN'       colorBgc='green'       colorText='white' />
+            <ButtonConstructor name='LIGHT BLUE'  colorBgc='dodgerblue'  colorText='white' />
+            <ButtonConstructor name='BLUE'        colorBgc='blue'        colorText='white' />
+            <ButtonConstructor name='PURPLE'      colorBgc='purple'      colorText='white' />
+            <ButtonConstructor name='BLACK'       colorBgc='black'       colorText='white' />
+            <ButtonConstructor name='WHITE'       colorBgc='white'       colorText='black' />
+            <ButtonConstructor name='GREY'        colorBgc='grey'        colorText='white' />
+            { clock ? <Clock bgc={color}/> : <Hello/> }
+        </div>
+    );
 }
 
 export default Time;
--- src/components/clock/Clock.jsx
+++ src/components/clock/Clock.jsx
@@ -1,77 +1,34 @@
 import React, {useState} from "react";
+import Hello from "../Hello";
 import css from './Clock.module.css';
+import './ClockColors.css';
 
-function Clock(props) {
+const Clock = props => {
 
+    let text = 'Current time and date:';
     let time = new Date().toLocaleTimeString();
     let date = new Date().toDateString();
-    let text = 'Current time and date:';
 
-    const [currentTime, setCurrentTime] = useState(time);
+    const [ currentTime, setCurrentTime ] = useState(time);
 
     const timeUpdate = () => {
         time = new Date().toLocaleTimeString();
         setCurrentTime(time);
     };
 
-    const returnJSX = <div className={css.wrapper}>
-        <h3>{text}</h3>
-        <h1>{time}</h1>
-        <h2>{date}</h2>
-    </div>
+    const returnJSX =   <div className={css.wrapper}>
+                            <h3>{text}</h3>
+                            <h1>{time}</h1>
+                            <h2>{date}</h2>
+                        </div>
 
     setInterval(timeUpdate, 1000);
 
-    if (props.bgc === 'red') {
-        return <div className={css.red}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'orange') {
-        return <div className={css.orange}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'yellow') {
-        return <div className={css.yellow}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'green') {
-        return <div className={css.green}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'dodgerblue') {
-        return <div className={css.dodgerblue}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'blue') {
-        return <div className={css.blue}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'purple') {
-        return <div className={css.dodgerblue}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'black') {
-        return <div className={css.black}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'white') {
-        return <div className={css.dodgerblue}>
-            {returnJSX}
-        </div>
-    }
-    if (props.bgc === 'grey') {
-        return <div className={css.grey}>
-            {returnJSX}
-        </div>
-    }
-}
+    if (props.bgc) {
+            return  <div className={props.bgc}>
+                        {returnJSX}
+                    </div>
+            } else return <Hello/>;
+};
 
 export default Clock;
